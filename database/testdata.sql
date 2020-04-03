a350eee138d921d1649524fee40335045e16775a
Jani Mäkinen <26940968+janimakinen@users.noreply.github.com>
2020-04-04 02:10:27 +0300

    Added some testdata to automated testing later on.

---
+++ database/testdata.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.applesignin
+(userid, email, "name")
+VALUES('testuserid1', 'testemail2@testemail1.com', 'testname1');
+
+INSERT INTO public.applesignin
+(userid, email, "name")
+VALUES('testuserid2', 'testemail2@testemail2.com', 'testname2');
